45f8d5ec45431b9ebdedf1e19628c388f2aba17a
cvicens <carlos.vicens.alonso@gmail.com>
2022-12-01 09:37:57 +0100

    version 1.0.0 includes argocd and yq

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export REGISTRY=quay.io
+export REGISTRY_USER_ID=atarazana
+export IMAGE_NAME=web-terminal-tooling
+export IMAGE_VERSION=1.0.0
+
+podman tag localhost/web-terminal-tooling:local quay.io/$REGISTRY_USER_ID/$IMAGE_NAME:$IMAGE_VERSION
+podman push $REGISTRY/$REGISTRY_USER_ID/$IMAGE_NAME:$IMAGE_VERSION
